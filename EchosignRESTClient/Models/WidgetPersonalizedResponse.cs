999d4079c0f599007be289c353994f36c24bec0a
Giorgos Trichopoulos <giorgost@medialink.com.cy>
2017-01-30 16:00:19 +0200

    Get full WidgetPersonalizedResponse

--- EchosignRESTClient/Models/WidgetPersonalizedResponse.cs
+++ EchosignRESTClient/Models/WidgetPersonalizedResponse.cs
@@ -9,8 +9,18 @@ namespace EchosignRESTClient.Models
     public class WidgetPersonalizedResponse
     {
         /// <summary>
-        /// Javascript of personalized widget
+        ///  Javascript snippet suitable for an embedded page taking a user to a URL
         /// </summary>
         public string javascript { get; set; }
+
+        /// <summary>
+        /// Standalone URL to direct end users to
+        /// </summary>
+        public string url { get; set; }
+
+        /// <summary>
+        /// The unique identifier of widget which can be used to retrieve the data entered by the signers
+        /// </summary>
+        public string widgetId { get; set; }
     }
 }
